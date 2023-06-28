6b53e0433e05a27e8dcf6cc435a4a7fd18d9fb71
sannytee <tywosanni@gmail.com>
2023-06-28 05:27:24 -0400

    fixes: existing query fails for sample provided in docs

--- instapro_dbt/models/analysis/availability_snapshot.sql
+++ instapro_dbt/models/analysis/availability_snapshot.sql
@@ -1,34 +1,44 @@
--- this model can be changed to use the incremental model. that's the ideal case.
-with active_professionals as (
+with event_date_diff as (
     select
-        professional_id_anonymized as professional_id,
-        created_at as created_at
+        professional_id_anonymized,
+        created_at,
+        event_type,
+        lead(created_at) over (partition by professional_id_anonymized order by created_at) as next_date
     from {{ source('events_logs', 'events') }}
+    where event_type in ('became_able_to_propose', 'became_not_able_to_propose')
+),
+filtered_date_dim as (
+    select
+        Date(dd.calendar_date) as date
+    from {{ ref('dim_date') }} dd
+    where DATE(dd.calendar_date) >= (select Date(min(created_at)) from events) AND DATE(dd.calendar_date) <= '2020-03-10'
+),
+join_calendar_dim_with_edd as (
+    select
+        dd.date,
+        edd.*
+    from filtered_date_dim dd
+    join event_date_diff edd on dd.date < coalesce(DATE(edd.next_date), '2020-03-11') and Date(dd.date) >= Date(edd.created_at)
+),
+active_professionals_per_day as (
+    select date,
+           count(distinct professional_id_anonymized) as active_professionals
+    from join_calendar_dim_with_edd
     where event_type = 'became_able_to_propose'
-     and DATE(created_at) <= (select DATE(max(created_at)) from events)
-     and (created_at, professional_id_anonymized) not in (
-     	select
-     	  min(created_at), professional_id_anonymized
-     	from events
-     	where event_type = 'became_unable_to_propose' and Date(created_at) <= (select DATE(max(created_at)) from events)
-     	group by professional_id_anonymized
-     )
+    group by date
 ),
-date_range as (
+days_with_zero_active_professionals as (
     select
-        date(calendar_date) as calendar_date
-    from {{ ref('dim_date') }}
-    where Date(calendar_date) <= '2020-03-10'
-),
-dates_with_active_professionals as (
-    select
-        calendar_date as day,
-        count(distinct p.professional_id) as active_professionals
-    from date_range d
-    join active_professionals p on date(p.created_at) <= d.calendar_date
-    group by calendar_date
+       date,
+      0 as active_professionals
+    from filtered_date_dim dd
+    where date not in (
+        select date from active_professionals_per_day
+    )
 )
 select
     *
-from dates_with_active_professionals
-
+from days_with_zero_active_professionals
+union all
+select * from active_professionals_per_day
+order by date
